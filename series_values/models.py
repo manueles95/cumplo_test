14e38cc0ce32f6ccccb9e67d8d340188d4dd8ee7
manueles95 <j.manuel_e@hotmail.com>
2019-12-22 03:31:38 -0600

    created model and fixtures for tiie index

--- series_values/models.py
+++ series_values/models.py
@@ -5,3 +5,8 @@ class SeriesIndex(models.Model):
     series_id = models.CharField(max_length=10, primary_key=True)
     front_identifier = models.CharField(max_length=3)
     description = models.CharField(max_length=100)
+
+class TiieIndex(models.Model):
+    tiie_id = models.CharField(max_length=10, primary_key=True);
+    term = models.IntegerField()
+    description = models.CharField(max_length=100)
